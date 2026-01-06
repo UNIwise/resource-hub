b8fd4c829ae0fb2cd1aaaf6d5514a60d07670ab1
Jonas Tranberg <jonastranberg93@gmail.com>
2026-01-06 22:56:32 +0100

    added unfonts from improved font loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import react from "@vitejs/plugin-react";
+import Unfonts from "unplugin-fonts/vite";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    Unfonts({
+      fontsource: {
+        families: [
+          {
+            name: "Mona Sans",
+            weights: [200, 300, 400, 500, 600, 700, 800, 900],
+          },
+          {
+            name: "JetBrains Mono",
+            weights: [200],
+          },
+        ],
+      },
+    }),
+    react(),
+  ],
 });
